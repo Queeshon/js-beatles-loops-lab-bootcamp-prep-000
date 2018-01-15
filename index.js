47fb676ef9bf01fe49325e3c529660272b107cb7
Christian Kim <christiandwkim@gmail.com>
2018-01-15 19:55:21 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function theBeatlesPlay (musicians, instruments){
+  var array = []
+  for (var i = 0; i < musicians.length; i++){
+    array.push ('${musicians[i]} plays ${instruments[i]}.')
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  var count = 0;
+  var array = []
+  while (count < facts.length){
+    array.push (`${facts}!!!`)
+  }
+  return array
+}
+
+function iLoveTheBeatles(number){
+  var array = []
+  do{
+    array.push (`I love the Beatles!`)
+  } while (array.length < 15 && array.length++){
+    array.push (`I love the Beatles!`)
+  }
+  return array
+}
